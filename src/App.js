6fc98113dcd81341d56922ab118e639aa8b19154
jaydenpiao <jaydenpiao@gmail.com>
2023-05-23 17:39:55 -0700

    Created word counter and frequency counter

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
+import React from 'react';
+import WordCount from './WordCount';
 import './App.css';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <div className="App">
+        <h1>Word Count Tool</h1>
+        <WordCount />
+      </div>
   );
 }
 
---
+++ src/WordCount.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+
+const WordCount = () => {
+    const [text, setText] = useState('');
+    const [wordCount, setWordCount] = useState(null);
+    const [wordFrequency, setWordFrequency] = useState({});
+
+    const countWords = () => {
+        const cleanedText = text.toLowerCase().replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, '');
+        const words = cleanedText.split(/\s+/).filter(Boolean);
+
+        setWordCount(words.length);
+
+        let frequency = {};
+        for (let word of words) {
+            if (frequency[word]) {
+                frequency[word]++;
+            } else {
+                frequency[word] = 1;
+            }
+        }
+
+        setWordFrequency(frequency);
+    };
+
+    return (
+        <div>
+            <textarea rows="10" cols="50" value={text} onChange={(e) => setText(e.target.value)} placeholder="Enter text here..."></textarea>
+            <button onClick={countWords}>Count Words</button>
+            <div>Total Words: {wordCount}</div>
+            <div>
+                {Object.entries(wordFrequency).map(([word, count]) => (
+                    <p key={word}>{word}: {count}</p>
+                ))}
+            </div>
+        </div>
+    );
+};
+
+export default WordCount;
